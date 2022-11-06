fe10a671b6550346787975cab94a25df6694e216
prithvirohira8 <prithvirohira8@gmail.com>
2022-11-06 12:34:00 +0530

    :twisted_rightwards_arrows: Merged feat-thread-summary branch with main

--- flaskBackend/profileSummarizer/profileSumm.py
+++ flaskBackend/profileSummarizer/profileSumm.py
@@ -1,34 +1,36 @@
+import sys
 import tweepy
-from dotenv import dotenv_values
-import re
 import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-
-dotenv_path = join(os.pardir, '.env')
-load_dotenv(dotenv_path)
-
-#API
-auth = tweepy.OAuthHandler(consumer_key=os.environ.get('consumer_key'),
-                           consumer_secret=os.environ.get('consumer_secret'))
-auth.set_access_token(os.environ.get('access_token'),
-                      os.environ.get('access_token_secret'))
-api = tweepy.API(auth)
-
-# Client
-client = tweepy.Client(os.environ.get('bearer_token'), consumer_key=os.environ.get('api_key'), consumer_secret=os.environ.get('api_key_secret'),
-                       access_token=os.environ.get('access_token'), access_token_secret=os.environ.get('access_token_secret'))
+backend_path = os.path.abspath(os.pardir).replace('\\', '\\\\')
+sys.path.insert(0, backend_path)
+from tweepy_cred import api, client
 
 def profile_summarizer(username):
-    obj = api.get_user(screen_name=username)
-    print(obj._json)
-    name = obj._json['name']
-    description = obj._json['description']
-    followers = obj._json['followers_count']
-    profile_image_url = obj._json['profile_image_url_https']
+    user_obj = api.get_user(screen_name=username)
+    name = user_obj._json['name']
+    description = user_obj._json['description']
+    followers = user_obj._json['followers_count']
+    profile_image_url = user_obj._json['profile_image_url_https']
 
-    user_tweets
-    mention_tweets
+    user_tweets_data = api.user_timeline(screen_name=username, count=1, tweet_mode="extended")
+    user_tweets = []
+    for tweet in user_tweets_data:
+        user_tweets.append(tweet._json['full_text'])
 
+    q = "@{0} and -filter:retweets".format(username)
+    mention_tweets_data = api.search_tweets(
+        q=q, count=1, tweet_mode="extended")
+    mention_tweets = []
+    for tweet in mention_tweets_data:
+        mention_tweets.append(tweet._json['full_text'])
 
-profile_summarizer("warikoo")
+    res_obj = {
+        "username": name,
+        "description": description,
+        "followers_count": followers,
+        "profile_image_url": profile_image_url,
+        "user_tweets": user_tweets,
+        "mention_tweets": mention_tweets
+    }
+
+    return res_obj
