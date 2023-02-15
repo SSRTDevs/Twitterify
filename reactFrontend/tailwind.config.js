b6492a64d08fce67ddd28ebf85dbaaf46eeeee7c
saket <saketthota98@gmail.com>
2023-02-15 21:13:58 +0530

    Merge with Twitterify-v2

--- reactFrontend/tailwind.config.js
+++ reactFrontend/tailwind.config.js
@@ -1,14 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
-    ],
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {
             fontFamily: {
-                'body': ["-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica", "Arial", "sans-serif", "Oxygen", "Ubuntu", "Cantarell"]
-            }
+                body: [
+                    "-apple-system",
+                    "BlinkMacSystemFont",
+                    "Segoe UI",
+                    "Roboto",
+                    "Helvetica",
+                    "Arial",
+                    "sans-serif",
+                    "Oxygen",
+                    "Ubuntu",
+                    "Cantarell",
+                ],
+            },
         }
     },
-    plugins: [require('@tailwindcss/line-clamp'),require("daisyui")],
-}
+    plugins: [require("@tailwindcss/line-clamp"), require("daisyui")],
+};
