ce07bf36d514ab0be8c04a24a310f2434cff23f9
Abhishek Sharma <abhishekalien09@gmail.com>
2023-02-15 22:33:14 +0530

    Update twitter-color

--- reactFrontend/tailwind.config.js
+++ reactFrontend/tailwind.config.js
@@ -1,14 +1,58 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                'body': ["-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica", "Arial", "sans-serif", "Oxygen", "Ubuntu", "Cantarell"]
-            }
-        }
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica",
+          "Arial",
+          "sans-serif",
+          "Oxygen",
+          "Ubuntu",
+          "Cantarell",
+        ],
+      },
     },
-    plugins: [require('@tailwindcss/line-clamp'),require("daisyui")],
-}
+    colors: {
+      twitter: {
+        100: "#159ff2",
+        200: "#1da1f2",
+      },
+      red: {
+        300: "#FCA5A5",
+        400: "#F87171",
+        500: "#EF4444",
+        600: "#DC2626",
+        700: "#B91C1C",
+        800: "#991B1B",
+        900: "#7F1D1D",
+      },
+      yellow: {
+        200: "#FDE68A",
+        300: "#FCD34D",
+        400: "#FBBF24",
+        500: "#F59E0B",
+        600: "#D97706",
+        700: "#B45309",
+        800: "#92400E",
+        900: "#78350F",
+      },
+      green: {
+        200: "#A7F3D0",
+        300: "#6EE7B7",
+        400: "#34D399",
+        500: "#10B981",
+        600: "#059669",
+        700: "#047857",
+        800: "#065F46",
+        900: "#064E3B",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/line-clamp"), require("daisyui")],
+};
