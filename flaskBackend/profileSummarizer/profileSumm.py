001030091ad4739df64b500769d6e80e308faad9
Shashwat <sataoshashwat0@gmail.com>
2023-02-16 14:59:54 +0530

    For pvt account result is pvt account

--- flaskBackend/profileSummarizer/profileSumm.py
+++ flaskBackend/profileSummarizer/profileSumm.py
@@ -1,5 +1,4 @@
 from setups.tweepy_cred import api
-<<<<<<< HEAD
 import calendar
 from datetime import datetime
 import tweepy
@@ -93,31 +92,6 @@ def profile_summarizer(username):
         name = user_obj._json['name']
         description = user_obj._json['description']
         followers = format_followers_count(user_obj._json['followers_count'])
-=======
-import tweepy
-
-#import sys
-#sys.path.insert(0,"/Users/shashwat/Twitterify/flaskBackend/setups" )
-#from tweepy_cred import api
-
-def profile_summarizer(username):
-    try:
-        user_obj = api.get_user(screen_name=username)
-        name = user_obj._json['name']
-        description = user_obj._json['description']
-        followers = user_obj._json['followers_count']
-        profile_image_url = user_obj._json['profile_image_url_https']
-
-        user_tweets_data = api.user_timeline(screen_name=username, count=1, tweet_mode="extended")
-        print(user_tweets_data)
-        user_tweets = []
-        for tweet in user_tweets_data:
-            user_tweets.append(tweet._json['full_text'])
-        user_obj = api.get_user(screen_name=username)
-        name = user_obj._json['name']
-        description = user_obj._json['description']
-        followers = user_obj._json['followers_count']
->>>>>>> b78c509 (Fixed for private accounts)
         profile_image_url = user_obj._json['profile_image_url_https']
         created_at = user_obj._json['created_at']
         arr = created_at.split()
@@ -140,6 +114,7 @@ def profile_summarizer(username):
             mention_tweets.append(tweet._json['full_text'])
 
         res_obj = {
+            "result": "success",
             "username": name,
             "description": description,
             "followers_count": followers,
@@ -150,13 +125,13 @@ def profile_summarizer(username):
         }
     except tweepy.errors.Unauthorized:
         res_obj = {
-            "username": "private_account",
-            "description": "private_account",
-            "followers_count": "private_account",
-            "created_at": "private_account",
-            "profile_image_url": "private_account",
-            "user_tweets": "private_account",
-            "mention_tweets": "private_account"
+            "result":"private_account",
+            "username": None,
+            "description": None,
+            "followers_count": None,
+            "created_at": None,
+            "profile_image_url": None,
+            "user_tweets": None,
+            "mention_tweets": None
         }
-        
     return res_obj
