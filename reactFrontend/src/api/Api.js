90acac0c1505f59016a0e69cf7332a11b86b389d
saket <saketthota98@gmail.com>
2023-02-27 01:31:22 +0530

    Implement search by hastag api in frontEnd

--- reactFrontend/src/api/Api.js
+++ reactFrontend/src/api/Api.js
@@ -1,84 +1,87 @@
 import axios from "axios";
 
-const trending = async (trends, setTrends, setAlert) => {
-  await axios
-    .get(`http://localhost:5000/trending_tweets`)
-    .then((res) => {
-      console.log("Trending Tweets", res.data);
-      setTrends((trends) => {
-        return { ...trends, latest_trends: res.data };
-      });
-    })
-    .catch((err) => {
-      console.log("Kuch toh gadbad hai beta");
-      setAlert({
-        error: "Error with Api-call",
-        bg: "bg-error",
-      });
-      setTimeout(() => {
-        setAlert({});
-      }, 2000);
+const trending = async (trends, setTrends, setAlert, topic) => {
+    console.log(topic);
+    await axios
+        .get(`http://localhost:5000/trending_tweets`)
+        .then((res) => {
+            console.log("Trending Tweets", res.data);
+            setTrends((trends) => {
+                return { ...trends, latest_trends: res.data };
+            });
+        })
+        .catch((err) => {
+            console.log("Kuch toh gadbad hai beta");
+            setAlert({
+                error: "Error with Api-call",
+                bg: "bg-error",
+            });
+            setTimeout(() => {
+                setAlert({});
+            }, 2000);
+        });
+
+    await axios.get(`http://localhost:5000/trending_tweets_by_topic`, { params: { topic: topic } }).then((res) => {
+        console.log("Trending tweets by topic", topic);
     });
 };
 
 const user_summarizer = async (user, setUser, setAlert) => {
-  await axios
-    .get(`http://localhost:5000/sentiments/${user.Username}/${user.tweets}`)
-    .then((res) => {
-      console.log(res.data)
-      res.data["sentiments"] = JSON.parse(res.data["sentiments"]);
-      console.log(res.data.payload)
-      setUser((user) => {
-        return { ...user, details: res.data };
-      });
-      console.log(user)
-    })
-    .catch((err) => {
-      console.log("Kuch toh gadbad hai beta");
-      setAlert({
-        error: "You might have entered wrong username",
-        bg: "bg-error",
-      });
-      setTimeout(() => {
-        setAlert({});
-      }, 3000);
-    });
+    await axios
+        .get(`http://localhost:5000/sentiments/${user.Username}/${user.tweets}`)
+        .then((res) => {
+            console.log(res.data);
+            res.data["sentiments"] = JSON.parse(res.data["sentiments"]);
+            console.log(res.data.payload);
+            setUser((user) => {
+                return { ...user, details: res.data };
+            });
+            console.log(user);
+        })
+        .catch((err) => {
+            console.log("Kuch toh gadbad hai beta");
+            setAlert({
+                error: "You might have entered wrong username",
+                bg: "bg-error",
+            });
+            setTimeout(() => {
+                setAlert({});
+            }, 3000);
+        });
 
-  await axios
-    .get(`http://localhost:5000/wordclouds/${user.Username}/${user.tweets}`)
-    .then((res) => {
-      let data = JSON.parse(JSON.stringify(res.data));
-      setUser((user) => {
-        return { ...user, clouds: data };
-      });
-    })
-    .catch((err) => {
-      console.log("Kuch toh gadbad hai beta");
-    });
+    await axios
+        .get(`http://localhost:5000/wordclouds/${user.Username}/${user.tweets}`)
+        .then((res) => {
+            let data = JSON.parse(JSON.stringify(res.data));
+            setUser((user) => {
+                return { ...user, clouds: data };
+            });
+        })
+        .catch((err) => {
+            console.log("Kuch toh gadbad hai beta");
+        });
 };
 
 const thread_summarizer = async (thread, setThread, setAlert) => {
-  console.log(thread.url);
-  await axios
-    .get(
-      `http://localhost:5000/thread_summary/${thread.url.replaceAll("/", "*")}}`
-    )
-    .then((res) => {
-      console.log(res.data);
-      setThread((thread) => {
-        return { ...thread, details: res.data };
-      });
-    })
-    .catch((err) => {
-      console.log("Kuch toh gadbad hai beta");
-      setAlert({
-        error: "Bruh !! Somethings wrong with your api. Fix it quick",
-        bg: "bg-error",
-      });
-      setTimeout(() => {
-        setAlert({});
-      }, 3000);
-    });
+    console.log(thread.url);
+    await axios
+        .get(`http://localhost:5000/thread_summary/${thread.url.replaceAll("/", "*")}}`)
+        .then((res) => {
+            console.log(res.data);
+            setThread((thread) => {
+                return { ...thread, details: res.data };
+            });
+        })
+        .catch((err) => {
+            console.log("Kuch toh gadbad hai beta");
+            setAlert({
+                error: "Bruh !! Somethings wrong with your api. Fix it quick",
+                bg: "bg-error",
+            });
+            setTimeout(() => {
+                setAlert({});
+            }, 3000);
+        });
 };
 
 export { trending, user_summarizer, thread_summarizer };
--- reactFrontend/src/components/Tabs.js
+++ reactFrontend/src/components/Tabs.js
@@ -6,46 +6,47 @@ export default function Tabs({ tabs }) {
   const [activeTabIndex, setActiveTabIndex] = useState(2);
 
   return (
-    <>
-      <div className='god-container grid grid-cols-6 h-screen '
-      data-theme="black">
-        <div className='left-section h-screen col-span-4 flex flex-col w-full p-2'>
-          <div className='top-container flex justify-between items-center p-4 mb-2'>
-            <div className='avatar'>
-              <div className='w-10 rounded-full'>
-                <img src={twitter_logo} />
-              </div>
-            </div>
-            <div className='tabs'>
-              {tabs.map((tab, index) => (
-                <a
-                  key={index}
-                  className={`tab tab-lg tab-lifted w-52 space-x-3
+      <>
+          <div className="god-container grid grid-cols-6 h-screen " data-theme="black">
+              <div className="left-section h-screen col-span-4 flex flex-col w-full p-2">
+                  <div className="top-container flex justify-between items-center p-4 mb-2">
+                      <div className="avatar">
+                          <div className="w-10 rounded-full">
+                              <img src={twitter_logo} alt="Twitter Logo"/>
+                          </div>
+                      </div>
+                      <div className="tabs">
+                          {tabs.map((tab, index) => (
+                              <a
+                                  key={index}
+                                  className={`tab tab-lg tab-lifted w-52 space-x-3
                                 ${activeTabIndex == index ? "tab-active" : ""}`}
-                  onClick={() => setActiveTabIndex(index)}>
-                  <span>{tab.icon}</span>
-                  <span>{tab.name}</span>
-                </a>
-              ))}
-            </div>
-            <button
-              className='btn btn-outline hover:bg-twitter-200 border-twitter-100 hover:border-twitter-200'
-              onClick={() => {
-                tabs[activeTabIndex].details();
-              }}>
-              Twiterify
-            </button>
-          </div>
+                                  onClick={() => setActiveTabIndex(index)}
+                              >
+                                  <span>{tab.icon}</span>
+                                  <span>{tab.name}</span>
+                              </a>
+                          ))}
+                      </div>
+                      <button
+                          className="btn btn-outline font-bold hover:font-bold hover:bg-twitter-200 border-twitter-100 hover:border-twitter-200 uppercase"
+                          onClick={() => {
+                              tabs[activeTabIndex].details();
+                          }}
+                      >
+                          Twiterify
+                      </button>
+                  </div>
 
-          <div className='left-container h-fit overflow-y-scroll items-start text-center p-1'>
-            {tabs[activeTabIndex].leftComponent}
-          </div>
-        </div>
+                  <div className="left-container h-fit overflow-y-scroll items-start text-center p-1">
+                      {tabs[activeTabIndex].leftComponent}
+                  </div>
+              </div>
 
-        <div className='right-container overflow-y-scroll h-screen col-span-2 p-5 w-full text-center'>
-          {tabs[activeTabIndex].rightComponent}
-        </div>
-      </div>
-    </>
+              <div className="right-container overflow-y-scroll h-screen col-span-2 p-5 w-full text-center">
+                  {tabs[activeTabIndex].rightComponent}
+              </div>
+          </div>
+      </>
   );
 }
